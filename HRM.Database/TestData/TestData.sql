749621c13285936cd4abd2ab9491df1ef6dcdba5
DmytroJK <punpundead@gmail.com>
2017-02-14 13:58:21 +0200

    q

--- HRM.Database/TestData/TestData.sql
+++ HRM.Database/TestData/TestData.sql
@@ -15,8 +15,8 @@ INSERT INTO [hrm].[OfficialHollidays] VALUES (5,'05-01-2017','Workers Day')
 INSERT INTO [hrm].[OfficialHollidays] VALUES (6,'05-09-2017','Victory Day')
 INSERT INTO [hrm].[OfficialHollidays] VALUES (7,'06-04-2017','Trinity')
 INSERT INTO [hrm].[OfficialHollidays] VALUES (8,'06-28-2017','Constitution Day of Ukraine')
-INSERT INTO [hrm].[OfficialHollidays] VALUES (7,'08-24-2017','Independence Day of Ukraine')
-INSERT INTO [hrm].[OfficialHollidays] VALUES (7,'10-14-2017','Day of defenders of Ukraine')
+INSERT INTO [hrm].[OfficialHollidays] VALUES (9,'08-24-2017','Independence Day of Ukraine')
+INSERT INTO [hrm].[OfficialHollidays] VALUES (10,'10-14-2017','Day of defenders of Ukraine')
 
 
 -- Settings data
