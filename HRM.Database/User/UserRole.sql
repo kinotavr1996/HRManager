eeb7a6a664a28c52c1a3bbad401fff6ca1766e49
Vasyl <gulpak@bk.ru>
2017-02-14 13:18:57 +0200

    fix

--- HRM.Database/User/UserRole.sql
+++ HRM.Database/User/UserRole.sql
@@ -4,10 +4,8 @@
 	[RoleId] int NOT NULL
 	CONSTRAINT [PK_UserRole] PRIMARY KEY ([Id])
 	CONSTRAINT [FK_UserRole] FOREIGN KEY ([RoleId]) REFERENCES [hrm].[Role]([Id])
-<<<<<<< HEAD
 	CONSTRAINT [FK_UserRoleUserId] FOREIGN KEY ([UserId]) REFERENCES [hrm].[User]([Id])
-=======
 	CONSTRAINT [FK_UserRole_UserId] FOREIGN KEY ([UserId]) REFERENCES [hrm].[User]([Id])
->>>>>>> 5a0f6391b6de5ecd7ca51fd0a4b0401b3c132339
+
 
 )
