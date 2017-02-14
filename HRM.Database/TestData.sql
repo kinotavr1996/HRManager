04abf67ea3f3f5baddbe04bd5d1b236d7cfdbb3b
Valeriy <gulpak95@mail.ru>
2017-02-14 11:43:17 +0200

    Status

---
+++ HRM.Database/TestData.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [hrm].[StatusType](Name) VALUES ('Vacation')
+INSERT INTO [hrm].[StatusType](Name) VALUES ('Sickness')
+
+INSERT INTO [hrm].[Status](Name,StatusTypeID) VALUES ('Junior',1)
+INSERT INTO [hrm].[Status](Name,StatusTypeID) VALUES ('Middle',2)
+INSERT INTO [hrm].[Status](Name,StatusTypeID) VALUES ('Senior',1)
+INSERT INTO [hrm].[Status](Name,StatusTypeID) VALUES ('HR',2)
