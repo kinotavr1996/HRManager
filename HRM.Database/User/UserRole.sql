2df9a4b4c3e1ff2bc47caf9d38e7db832ab4cdcc
Freteuchan <fretuchan@gmail.com>
2017-02-14 11:57:15 +0200

    add user models

---
+++ HRM.Database/User/UserRole.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [hrm].[UserRole](
+	[Id] int NOT NULL,
+	[UserId] int NOT NULL,
+	[RoleId] int NOT NULL
+	CONSTRAINT [PK_UserRole] PRIMARY KEY ([Id])
+	CONSTRAINT [FK_UserRole] FOREIGN KEY ([RoleId]) REFERENCES [hrm].[Role]([Id])
+	CONSTRAINT [FK_UserRoleUserId] FOREIGN KEY ([UserId]) REFERENCES [hrm].[User]([Id])
+
+)
