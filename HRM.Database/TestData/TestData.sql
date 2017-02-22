3afa5467b4920b21578d7f0a39b337a39bdb165a
Vasyl <gulpak@bk.ru>
2017-02-22 10:48:00 +0200

    [+] DataBase

--- HRM.Database/TestData/TestData.sql
+++ HRM.Database/TestData/TestData.sql
@@ -1,11 +1,12 @@
-﻿INSERT INTO [hrm].[RequestType]([Id],[Name]) VALUES (1,'SickReques');
-INSERT INTO [hrm].[RequestType]([Id],[Name]) VALUES (2,'VacationRequst');
-
+﻿
+INSERT INTO [hrm].[Settings]([Id],[SickDays],[VacationDays]) VALUES (1,5,20);
 
 INSERT INTO [hrm].[UserLevel]([Id],[Name]) VALUES (1,'Junior');
-INSERT INTO [hrm].[UserLevel]([Id],[Name]) VALUES(2,'Middle');
-INSERT INTO [hrm].[UserLevel]([Id],[Name]) VALUES(3,'Senior');
+INSERT INTO [hrm].[UserLevel]([Id],[Name]) VALUES (2,'Middle');
+INSERT INTO [hrm].[UserLevel]([Id],[Name]) VALUES (3,'Senior');
 
+INSERT INTO [hrm].[RequestType]([Id],[Name]) VALUES (1,'SickReques');
+INSERT INTO [hrm].[RequestType]([Id],[Name]) VALUES (2,'VacationRequst');
 
 INSERT INTO [hrm].[OfficialHollidays]([Id],[Date],[Name]) VALUES (1,'01-01-2017','New Year');
 INSERT INTO [hrm].[OfficialHollidays]([Id],[Date],[Name]) VALUES (2,'01-07-2017','Christmas');
@@ -18,40 +19,38 @@ INSERT INTO [hrm].[OfficialHollidays]([Id],[Date],[Name]) VALUES (8,'06-28-2017'
 INSERT INTO [hrm].[OfficialHollidays]([Id],[Date],[Name]) VALUES (9,'08-24-2017','Independence Day of Ukraine');
 INSERT INTO [hrm].[OfficialHollidays]([Id],[Date],[Name]) VALUES (10,'10-14-2017','Day of defenders of Ukraine');
 
-
-
-INSERT INTO [hrm].[Settings]([Id],[SickDays],[VacationDays]) VALUES (1,5,20);
-
 INSERT INTO [hrm].[StatusType]([Id], [Name]) VALUES (1, 'User');
-INSERT INTO [hrm].[StatusType]([Id], Name) VALUES (2, 'Request');
+INSERT INTO [hrm].[StatusType]([Id], [Name]) VALUES (2, 'Request');
 
 INSERT INTO [hrm].[Status]([Id],[Name], [StatusTypeId]) VALUES (1,'Vacation',		1);
 INSERT INTO [hrm].[Status]([Id],[Name], [StatusTypeId]) VALUES (2,'Sickness',		2);
 INSERT INTO [hrm].[Status]([Id],[Name], [StatusTypeId]) VALUES (3,'In processing',	1);
 
-
-
 INSERT INTO [hrm].[Role]([Id],[Name]) VALUES (1,'User');
 INSERT INTO [hrm].[Role]([Id],[Name]) VALUES (2,'TeamLead');
 INSERT INTO [hrm].[Role]([Id],[Name]) VALUES (3,'HR');
 
-INSERT INTO [hrm].[Team]([TeamName]) VALUES ('Students');
-INSERT INTO [hrm].[Team]([TeamName]) VALUES ('HRM');
-INSERT INTO [hrm].[Team]([TeamName]) VALUES ('Barcelona');
+SET IDENTITY_INSERT [hrm].[Team] ON
+INSERT INTO [hrm].[Team]([Id],[TeamName]) VALUES (1,'Students');
+INSERT INTO [hrm].[Team]([Id],[TeamName]) VALUES (2,'HRM');
+INSERT INTO [hrm].[Team]([Id],[TeamName]) VALUES (3,'Barcelona');
+SET IDENTITY_INSERT [hrm].[Team] OFF
 
-INSERT INTO [hrm].[User]([FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES ('Grisha','grisha2017','grisha@mail.ru','2017-02-03',1,1 );
-INSERT INTO [hrm].[User]([FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES ('Oleg','oleg','oleg@mail.ru','2017-02-03',2,2 );
-INSERT INTO [hrm].[User]([FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES ('Andriy','andriy','andriy@mail.ru','2017-02-03',3,3 );
-INSERT INTO [hrm].[User]([FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES ('Dmytro','dmytro','dmytro@mail.ru','2017-02-03',1,1 );
-INSERT INTO [hrm].[User]([FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES ('Vasyl','vasyl','vasyl@mail.ru','2017-02-03',2,2 );
+SET IDENTITY_INSERT [hrm].[User] ON
+INSERT INTO [hrm].[User]([Id],[FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES (1,'Grisha','grisha2017','grisha@mail.ru','2017-02-03',1,1 );
+INSERT INTO [hrm].[User]([Id],[FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES (2,'Oleg','oleg','oleg@mail.ru','2017-02-03',2,2 );
+INSERT INTO [hrm].[User]([Id],[FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES (3,'Andriy','andriy','andriy@mail.ru','2017-02-03',3,3 );
+INSERT INTO [hrm].[User]([Id],[FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES (4,'Dmytro','dmytro','dmytro@mail.ru','2017-02-03',1,1 );
+INSERT INTO [hrm].[User]([Id],[FullName],[Password],[Email],[StartDate],[UserLevelId],[UserStatusId]) VALUES (5,'Vasyl','vasyl','vasyl@mail.ru','2017-02-03',2,2 );
+SET IDENTITY_INSERT [hrm].[User] OFF
 
-INSERT INTO [hrm].[UserTeam]([Id],[UserId],[TeamId]) VALUES (1,1,1);
-INSERT INTO [hrm].[UserTeam]([Id],[UserId],[TeamId]) VALUES (2,2,2);
-INSERT INTO [hrm].[UserTeam]([Id],[UserId],[TeamId]) VALUES (3,1,3);
+INSERT INTO [hrm].[UserTeam]([UserId],[TeamId]) VALUES (1,1);
+INSERT INTO [hrm].[UserTeam]([UserId],[TeamId]) VALUES (2,2);
+INSERT INTO [hrm].[UserTeam]([UserId],[TeamId]) VALUES (1,3);
 
-INSERT INTO [hrm].[UserRole]([Id],[UserId],[RoleId]) VALUES (1,1,1);
-INSERT INTO [hrm].[UserRole]([Id],[UserId],[RoleId]) VALUES (2,2,2);
-INSERT INTO [hrm].[UserRole]([Id],[UserId],[RoleId]) VALUES (3,3,3);
+INSERT INTO [hrm].[UserRole]([UserId],[RoleId]) VALUES (1,1);
+INSERT INTO [hrm].[UserRole]([UserId],[RoleId]) VALUES (2,2);
+INSERT INTO [hrm].[UserRole]([UserId],[RoleId]) VALUES (3,3);
 
 INSERT INTO [hrm].[UserDocument]([UserId],[DocumentLink]) VALUES (1,'vk');
 INSERT INTO [hrm].[UserDocument]([UserId],[DocumentLink]) VALUES (2,'mail');
